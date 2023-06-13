97bdae838ec6cb65a8aba2dee40f366116fad381
Kevin Wang <wang1kevin1@gmail.com>
2023-06-12 18:14:01 -0700

    I have no idea what I'm doing fuck JS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,14 @@
 	import Player from '../lib/Player.svelte'
 
 	let gearsetId = '1103c082-1c80-4bf3-bb56-83734971d5ea'
-	export let itemId = []
+	let equipmentId = []
+
 	const gearsetURL = 'https://etro.gg/gearset/'
 	const gearsetEndpoint = 'https://etro.gg/api/gearsets/'
-	const itemEndpoint = 'https://etro.gg/api/equipment/'
+	const equipmentEndpoint = 'https://etro.gg/api/equipment/'
 
 	export let gearset = new Map()
-	export let items = new Map()
+	export let equipment = new Map()
 
 	async function populateGearset() {
 		// grabs gearset and maps json response to gearsetID
@@ -20,40 +21,45 @@
 
 		gearset.set(gearsetId, data)
 
-		// populates an array of all itemIds needed based on the json data (without duplicates)
-		let set = [
-			data.weapon,
-			data.head,
-			data.body,
-			data.hands,
-			data.legs,
-			data.feet,
-			data.offHand,
-			data.ears,
-			data.neck,
-			data.wrists,
-			data.fingerL,
-			data.fingerR
-		]
-
-		set.forEach(item => {
-			if (item != null && !itemId.includes(item)) {
-				itemId.push(item)
-			}
-		})
 		return gearset
-
-		await Promise.all(
-			itemId.map(async itemId => {
-				const response = await fetch(itemEndpoint + itemId)
-				const data = await response.json()
-
-				items.set(itemId, data)
-			})
-		)
 	}
 
-	let promise1 = populateGearset()
+	async function populateEquipment(gearset) {
+		// populates an array of all itemIds needed based on the json data (without duplicates)
+		let set = [
+			gearset.get(gearsetId).weapon,
+			gearset.get(gearsetId).head,
+			gearset.get(gearsetId).body,
+			gearset.get(gearsetId).hands,
+			gearset.get(gearsetId).legs,
+			gearset.get(gearsetId).feet,
+			gearset.get(gearsetId).offHand,
+			gearset.get(gearsetId).ears,
+			gearset.get(gearsetId).neck,
+			gearset.get(gearsetId).wrists,
+			gearset.get(gearsetId).fingerL,
+			gearset.get(gearsetId).fingerR
+		]
+
+		set.forEach(equipment => {
+			if (equipment != null && !equipmentId.includes(equipment)) {
+				equipmentId.push(equipment)
+			}
+		})
+
+		await Promise.all(
+			equipmentId.map(async equipmentId => {
+				const response = await fetch(equipmentEndpoint + equipmentId)
+				const data = await response.json()
+
+				equipment.set(equipmentId, data)
+			})
+		)
+
+		return equipment
+	}
+
+	let promise1 = populateGearset().then(gearset => populateEquipment(gearset))
 </script>
 
 <div class="flex w-screen h-screen p-20">
@@ -69,31 +75,18 @@
 				</tr>
 			</thead>
 			<tbody>
-				{#await promise}
+				{#await promise1}
 					<tr class="border">
 						<td class="px-8 py-4">
 							<p>Loading...</p>
 						</td>
 					</tr>
-				{:then gearsets}
-					{#each gearsetId as i}
-						<Player
-							name={i}
-							link={gearsetURL + i}
-							weapon={items.get(gearsets.get(i).weapon).name}
-							head={items.get(gearsets.get(i).head).name}
-							body={items.get(gearsets.get(i).body).name}
-							hands={items.get(gearsets.get(i).hands).name}
-							legs={items.get(gearsets.get(i).legs).name}
-							feet={items.get(gearsets.get(i).feet).name}
-							offHand={items.get(gearsets.get(i).offHand).name}
-							ears={items.get(gearsets.get(i).ears).name}
-							neck={items.get(gearsets.get(i).neck).name}
-							wrists={items.get(gearsets.get(i).wrists).name}
-							fingerL={items.get(gearsets.get(i).fingerL).name}
-							fingerR={items.get(gearsets.get(i).fingerR).name}
-						/>
-					{/each}
+				{:then props}
+					<Player
+						name={gearsetId}
+						link={gearsetURL + gearsetId}
+						weapon={props.equipment.get(props.gearset.get(gearsetId).weapon).name}
+					/>
 				{:catch error}
 					<p class="text-red">{error.message}</p>
 				{/await}
@@ -101,3 +94,14 @@
 		</table>
 	</div>
 </div>
+<!-- head={equipment.get(gearsets.get(i).head).name}
+							body={equipment.get(gearsets.get(i).body).name}
+							hands={equipment.get(gearsets.get(i).hands).name}
+							legs={equipment.get(gearsets.get(i).legs).name}
+							feet={equipment.get(gearsets.get(i).feet).name}
+							offHand={equipment.get(gearsets.get(i).offHand).name}
+							ears={equipment.get(gearsets.get(i).ears).name}
+							neck={equipment.get(gearsets.get(i).neck).name}
+							wrists={equipment.get(gearsets.get(i).wrists).name}
+							fingerL={equipment.get(gearsets.get(i).fingerL).name}
+							fingerR={equipment.get(gearsets.get(i).fingerR).name} -->
