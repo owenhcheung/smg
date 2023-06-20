2ce80b3bebc791485d710b4bff6eadce413505e4
Owen <ocheung198@gmail.com>
2023-06-20 01:09:48 -0700

    fff

--- src/lib/Player.svelte
+++ src/lib/Player.svelte
@@ -4,19 +4,83 @@
 	export let head = 'head name'
 	export let body = 'body name'
 
-	export let link = 'input gearset'
+	let gearsetLink = ''
 
-	let id = link.replace(/\/\/*/g, '/').replace(/\/+$/, '').slice(23) // remove any trailing slash + get id
+	$: gearsetEndpoint =
+		'https://etro.gg/api/gearsets/' +
+		gearsetLink.replace(/\/\/*/g, '/').replace(/\/+$/, '').slice(23)
+	$: equipmentEndpoint = 'https://etro.gg/api/equipment/'
+
+	async function fetchGearset(gearset) {
+		try {
+			const response = await fetch(gearset)
+			const data = await response.json()
+
+			const {
+				weapon: weaponID,
+				head: headID,
+				body: bodyID,
+				hands: handID,
+				legs: legID,
+				feet: feetID,
+				ears: earID,
+				neck: neckID,
+				wrists: wristID,
+				fingerL: ringLID,
+				fingerR: ringRID
+			} = data
+
+			return [
+				weaponID,
+				headID,
+				bodyID,
+				handID,
+				legID,
+				feetID,
+				earID,
+				neckID,
+				wristID,
+				ringLID,
+				ringRID
+			]
+		} catch (error) {
+			console.error('something fucked up', error.message, error.stack)
+			return []
+		}
+	}
+
+	async function fetchItemName(item) {
+		const response = await fetch(item)
+		const data = await response.json()
+
+		return data.name
+	}
+
+	$: {
+		if (gearsetLink) {
+			let temp = []
+			fetchGearset(gearsetEndpoint).then(gearset => {
+				gearset.forEach(id =>
+					fetchItemName(equipmentEndpoint + id).then(name => {
+						temp.push(name)
+					})
+				)
+			})
+			console.log(temp)
+			// console.log(fetchItemName(equipmentEndpoint + '40206'))
+		}
+	}
 </script>
 
 <tr>
 	<td class="border px-8 py-4">
-		<div class="grid autogrid items-baseline">
+		<div class="flex items-baseline">
 			<p class="mr-5">{name}</p>
 			<input
 				type="text"
-				class="bg-stone-200 text-neutral-500 focus:text-black py-1 px-3 truncate focus:outline-none w-36"
-				bind:value={link}
+				class="bg-stone-200 text-neutral-500 focus:text-black py-1 px-3 truncate focus:outline-none w-40"
+				placeholder="input gearset link"
+				bind:value={gearsetLink}
 			/>
 		</div>
 	</td>
@@ -26,7 +90,4 @@
 </tr>
 
 <style>
-	.autogrid {
-		grid-template-columns: auto 1fr;
-	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,36 +2,13 @@
 </script>
 
 <script>
-	import { onMount } from 'svelte'
 	import Player from '../lib/Player.svelte'
-
-	let gearsetId = ['f2426d1e-2da8-4151-bf52-74ca67b5f4a2']
-
-	const gearsetURL = 'https://etro.gg/gearset/'
-	const gearsetEndpoint = 'https://etro.gg/api/gearsets/'
-	const equipmentEndpoint = 'https://etro.gg/api/equipment/'
-
-	export let gearsets = new Map()
-	//export let weapons = []
-
-	async function populateGearsets() {
-		await Promise.all(
-			gearsetId.map(async gearsetId => {
-				const response = await fetch(gearsetEndpoint + gearsetId)
-				const data = await response.json()
-
-				gearsets.set(gearsetId, data)
-			})
-		)
-		return gearsets
-	}
-
-	let promise = populateGearsets()
+	let link = ''
 </script>
 
 <div class="flex w-screen h-screen p-20">
 	<div class="w-full h-full">
-		<p class="text-2xl font-bold mb-5">Loot Table</p>
+		<!-- <p class="text-2xl font-bold mb-5">Loot Table</p> -->
 		<table class="table-auto w-full shadow-lg">
 			<thead>
 				<tr>
@@ -42,21 +19,8 @@
 				</tr>
 			</thead>
 			<tbody>
-				{#await promise}
-					<tr class="border">
-						<td class="px-8 py-4">
-							<p>Loading...</p>
-						</td>
-					</tr>
-				{:then gearsets}
-					<Player
-						name={'Warrior'}
-						link={'https://etro.gg/gearset/1103c082-1c80-4bf3-bb56-83734971d5ea'}
-					/>
-					<Player name={'Bard'} />
-				{:catch error}
-					<p class="text-red">{error.message}</p>
-				{/await}
+				<Player name={'Warrior'} />
+				<Player name={'Bard'} />
 			</tbody>
 		</table>
 	</div>
